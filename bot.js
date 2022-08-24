3af3e6e839c45bc3ed9bbe91ba3dbc9a4340b52d
Glenn Hofman <glennh1991@gmail.com>
2022-08-24 12:17:21 +0200

    added keyv

--- bot.js
+++ bot.js
@@ -15,6 +15,9 @@ client.commands = new Collection();
 const commands = [];
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
+const keyv = new Keyv('mysql://' + process.env.DBUSER + ':' + process.env.DBPASS + '@localhost:3306/tmsd_');
+
+keyv.on('error', err => console.error('Keyv connection error:', err));
 
 
 for (const file of commandFiles) {
